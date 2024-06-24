9b664947804a16978cb51cc945b35d9b5defde7c
Zvi Grinberg <zgrinber@redhat.com>
2024-06-24 15:33:49 +0300

    fix: add resourceName anyuid for scc  rbac permissions
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- controllers/morpheus_controller.go
+++ controllers/morpheus_controller.go
@@ -53,7 +53,7 @@ type MorpheusReconciler struct {
 //+kubebuilder:rbac:groups=*,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=*,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles;rolebindings,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=security.openshift.io,resources=securitycontextconstraints,verbs=use
+//+kubebuilder:rbac:groups=security.openshift.io,resources=securitycontextconstraints,resourceNames=anyuid,verbs=use
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
